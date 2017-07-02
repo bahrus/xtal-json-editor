0efd6460a4ed463fb6814d54b9b22d75b0e2e554
Bruce Anderson <anderson.bruce.b@gmail.com>
2017-07-01 20:51:03 -0400

    json editor

---
+++ xtal-json-editor.js
@@ -0,0 +1,48 @@
+///<reference path="polymer2.d.ts"/>
+/// <reference types="jsoneditor" />
+var xtal;
+(function (xtal) {
+    var elements;
+    (function (elements) {
+        customElements.whenDefined('dom-module').then(() => {
+            /**
+             * Polymer based web component wrapper around the JSON Editor api.
+             *
+             * Code can be found at https://github.com/josdejong/jsoneditor
+             */
+            class XtalJsonEditor extends Polymer.Element {
+                static get is() { return 'xtal-json-editor'; }
+                static get properties() {
+                    return {
+                        /**
+                        * The expression that points to an object to edit.
+                        */
+                        watch: {
+                            type: Object,
+                            observer: 'onPropsChange'
+                        },
+                        /**
+                         * JsonEditor options, implements jsoneditor.JSONEditorOptions
+                         */
+                        options: {
+                            type: Object,
+                            observer: 'onPropsChange'
+                        }
+                    };
+                }
+                get jsonEditor() {
+                    return this._jsonEditor;
+                }
+                onPropsChange(newVal) {
+                    if (!this.watch)
+                        return;
+                    this.$.xcontainer.innerHTML = '';
+                    this._jsonEditor = new JSONEditor(this.$.xcontainer, this.options);
+                    this._jsonEditor.set(this.watch);
+                }
+            }
+            customElements.define(XtalJsonEditor.is, XtalJsonEditor);
+        });
+    })(elements = xtal.elements || (xtal.elements = {}));
+})(xtal || (xtal = {}));
+//# sourceMappingURL=xtal-json-editor.js.map
