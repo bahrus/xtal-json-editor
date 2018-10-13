bb79a25aff97e407d7316338d9f9f696404babdd
bahrus <anderson.bruce.b@gmail.com>
2018-10-13 07:53:09 -0400

    ...

--- createIIFE.js
+++ createIIFE.js
@@ -1,6 +1,7 @@
 //@ts-check
 const jiife = require('jiife');
-jiife.processFiles(['node_modules/xtal-latx/xtal-latx.js','xtal-json-editor.js'], 'xtal-json-editor.iife.js');
+const xl = 'node_modules/xtal-latx/';
+jiife.processFiles([xl + 'define.js', xl + 'xtal-latx.js','xtal-json-editor.js'], 'xtal-json-editor.iife.js');
 
 
 
